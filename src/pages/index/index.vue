630f45b79aa1bce4af84f29839ae72f4fe29a43c
ShawnZhong <shawnzhongchn@gmail.com>
2025-08-31 16:13:12 +0800

    Initial commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <view class="homeLayout">
+    <view class="banner">
+      <swiper indicator-active-color="rgba(255,255,255,0.5)" indicator-dots="True" autoplay="True">
+        <swiper-item>
+          <image src="../../common/images/banner1.jpg" mode="aspectFill"></image>
+        </swiper-item>
+        <swiper-item>
+          <image src="../../common/images/banner1.jpg" mode="aspectFill"></image>
+        </swiper-item>
+        <swiper-item>
+          <image src="../../common/images/banner1.jpg" mode="aspectFill"></image>
+        </swiper-item>
+
+      </swiper>
+    </view>
+
+  </view>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.homeLayout {
+  .banner {
+    width: 750rpx;
+    padding: 30rpx 0;
+
+    swiper {
+      width: 750rpx;
+      height: 340rpx;
+
+      &-item {
+        width: 100%;
+        height: 100%;
+        padding: 0 30rpx;
+
+        image {
+          width: 100%;
+          height: 100%;
+          border-radius: 50rpx;
+        }
+      }
+    }
+  }
+}
+</style>
